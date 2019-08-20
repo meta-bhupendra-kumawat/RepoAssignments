a94f1d88bcfedf96d0435d8fc704616b90a9a1b4
meta-bhupendra-kumawat <52690533+meta-bhupendra-kumawat@users.noreply.github.com>
2019-08-20 18:50:21 +0530

    Add files via upload

---
+++ TopCategory.java
@@ -0,0 +1,21 @@
+
+public class TopCategory {
+	
+	// instance variables.
+	private String categoryName;
+	private int numberOfChilds;
+	
+	// Getters and setters.
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getNumberOfChilds() {
+		return numberOfChilds;
+	}
+	public void setNumberOfChilds(int numberOfChilds) {
+		this.numberOfChilds = numberOfChilds;
+	}
+}
